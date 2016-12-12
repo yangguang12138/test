85a3838db5aedea380a6ac5c0a222f732dd55c54
iOS_Onion <ios_onion@Mannys-MacPro.local>
2016-12-12 17:55:45 +0800

    nil

---
+++ test2/test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by iOS_Onion on 2016/12/12.
+//  Copyright © 2016年 iOS_Onion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by iOS_Onion on 2016/12/12.
+//  Copyright © 2016年 iOS_Onion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
